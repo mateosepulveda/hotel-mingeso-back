074df379323870c358d7ff60a5806bb2364a548e
matias <matias.pizarro@usach.cl>
2019-10-09 14:24:39 -0300

    init springboot maven project

---
+++ mingeso/src/main/java/com/example/mingeso/MingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingesoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingesoApplication.class, args);
+    }
+
+}
